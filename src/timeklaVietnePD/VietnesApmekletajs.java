dc4db6382fbd7c869e506e8cd926cb0b27897e85
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-10-22 15:08:35 +0300

    Izveidota Virsklase VietnesApmekletajs

---
+++ src/timeklaVietnePD/VietnesApmekletajs.java
@@ -0,0 +1,86 @@
+package timeklaVietnePD;
+
+import java.io.Serializable;
+import java.util.Random;
+
+
+public class VietnesApmekletajs implements Serializable, Comparable<VietnesApmekletajs>{
+	/**
+	 *  Ģenerēts serialVersionUID
+	 */
+	private static final long serialVersionUID = 5672751866842530800L;
+	
+		// Atribūti
+		private int DzGads, Nosutiti;
+		private String lietVards, parole, vards, uzvards, valsts;
+		
+		// Bezargumentu konstruktors
+		public VietnesApmekletajs() { };
+
+		
+		// Getter metodes
+		public String getVards() {
+			return vards;
+		}
+		
+		public String getUzvards() {
+			return uzvards;
+		}
+		
+		public String getLietVards() {
+			return lietVards;
+		}
+		
+		public String getParole() {
+			return parole;
+		}
+		
+		public String getValsts() {
+			return valsts;
+		}
+		
+		public int getDzGads() {
+			return DzGads;
+		}
+		
+		public int getNosutiti() {
+			return Nosutiti;
+		}
+		
+		
+		
+		// Setter metodes
+		public void setVards(String vards) {
+			this.vards = vards;
+		}
+		
+		public void setUzvards(String uzvards) {
+			this.uzvards = uzvards;
+		}
+		
+		public void setLietVards(String lietVards) {
+			this.lietVards = lietVards;
+		}
+		
+		public void setParole(String parole) {
+			this.parole = parole;
+		}
+		
+		public void setValsts(String valsts) {
+			this.valsts = valsts;
+		}
+		
+		public void setDzGads(int DzGads) {
+			this.DzGads = DzGads;
+		}
+		
+		public void setNosutiti(int Nosutiti) {
+			this.Nosutiti = Nosutiti;
+		}
+			
+		@Override
+		public int compareTo(VietnesApmekletajs o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+}
