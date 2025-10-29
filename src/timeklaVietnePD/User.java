df0341b7dac9cf17ce45ecaf05283e3466a80eaa
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-10-29 08:35:09 +0200

    Tika veikta neliela dzēšana

--- src/timeklaVietnePD/User.java
+++ src/timeklaVietnePD/User.java
@@ -3,13 +3,16 @@ package timeklaVietnePD;
 import timeklaVietnePD.VietnesApmekletajs;
 
 public class User extends VietnesApmekletajs{
+	
+	// Atribūti
+	//Nav
 
 	public User(int DzGads, int Nosutiti, String lietVards, String parole, String vards, String uzvards,
 			String valsts, String epasts) {
 		super(DzGads, Nosutiti, lietVards, parole, vards, uzvards, valsts, epasts);
 		// TODO Auto-generated constructor stub
 	}
-	// Atribūti
+	
 	
 	
 	
